aaed58215fdf70822b8464c544ec72c88c86c12c
Yim Klok <yimklok.kh@gmail.com>
2023-02-27 22:33:36 +0700

    new file

--- client/file.blade.php
+++ client/file.blade.php
@@ -16,30 +16,36 @@
             margin: 0;
             padding: 0;
             font-size: 2em;
-            width: 100vw;
-            height: 100vh;
+            width: 100dvw;
+            height: 100dvh;
             display: flex;
             justify-content: center;
             align-items: center;
             color: rgb(111, 111, 111);
         }
-
-        .d-flex {
+        .d-flex{
             display: flex;
             align-items: center;
             flex-direction: column;
         }
-
-        .d-flex h2 {
+        .d-flex h2{
             margin: 0;
             padding: 0;
+        }
+        .p-4{
+            padding: 1rem;
+        }
+        @media (max-width: 600px){
+            body{
+                font-size: 1rem;
+            }
         }
     </style>
 </head>
 
 <body>
-    <div class="d-flex">
-        <h2>File API</h2>
+    <div class="d-flex p-4">
+        <h2>POS FILE</h2>
         <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
     </div>
 </body>

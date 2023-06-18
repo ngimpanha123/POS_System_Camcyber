7579a49837fd3b922d4a0ae7e206800b90015f74
Khouch Koeun <khouch.koeun@gmail.com>
2023-06-19 01:08:45 +0700

    naming structure

--- routes/api.php
+++ routes/api.php
@@ -1,48 +1,49 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
 
-Route::group([], function () {
+
     
     //============>>  Auth
     Route::group(['prefix' => 'auth'], function () {
-        require(__DIR__ . '/api/auth/main.php');
+        require(__DIR__ . '/api/auth.php');
     });
+
     //============>>  Authenticated
     Route::group(['middleware' => ['jwt.verify']], function () {
 
         //============>>  Dashboard
-        require(__DIR__ . '/api/dashboard/main.php');
+        require(__DIR__ . '/api/dashboard.php');
 
         //============>>  POS
-        require(__DIR__ . '/api/pos/main.php');
+        require(__DIR__ . '/api/pos.php');
 
         //============>>  Sale
-        require(__DIR__ . '/api/sale/main.php');
+        require(__DIR__ . '/api/sale.php');
 
         //============>>  Product
-        require(__DIR__ . '/api/product/main.php');
+        require(__DIR__ . '/api/product.php');
 
         //============>>  User
-        require(__DIR__ . '/api/user/main.php');
+        require(__DIR__ . '/api/user.php');
 
         //============>> My Profile
-        require(__DIR__ . '/api/myprofile/main.php');
+        require(__DIR__ . '/api/myprofile.php');
     });
 
     //============>> PrintPDF
     Route::group(['prefix' => 'print'], function () {
-        require(__DIR__ . '/api/printpdf/main.php');
+        require(__DIR__ . '/api/printpdf.php');
     });
-});
+

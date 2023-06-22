3d41b1abfcfe887f425f9494b918d46487afd8ea
Khouch Koeun <khouch.koeun@gmail.com>
2023-06-22 23:57:06 +0700

    master

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'users';
+    protected $table = 'user';
     // Rest omitted for brevity
 
     public function getJWTIdentifier()

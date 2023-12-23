c7a354de369a07550e8a8fe4b09d934b84ae1262
Yim Klok <yimklok.kh@gmail.com>
2023-12-23 19:56:10 +0700

    modified .env.example and Dockerfile

--- client/file.blade.php
+++ client/file.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>POS API</title>
+    <title>POS FILE</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">

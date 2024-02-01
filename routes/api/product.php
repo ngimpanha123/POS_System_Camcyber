98068440209ece4d339c866bd9c164175c217726
Khouch Koeun <khouch.koeun@gmail.com>
2024-02-01 16:10:08 +0700

    remove product type

--- routes/api/product.php
+++ routes/api/product.php
@@ -4,17 +4,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Product\ProductController;
-use App\Http\Controllers\Product\ProductTypeController;
 
 Route::get('/products',         [ProductController::class, 'listing']); // Read Many Records
 Route::get('/products/{id}',    [ProductController::class, 'view']); // Read 1 Record
 Route::post('/products',        [ProductController::class, 'create']);
 Route::post('/products/{id}',   [ProductController::class, 'update']); // Update
 Route::delete('/products/{id}', [ProductController::class, 'delete']);
-
-Route::group(['prefix' => 'product'], function () {
-    Route::get('/types',            [ProductTypeController::class, 'listing']);
-    Route::post('/types',           [ProductTypeController::class, 'create']);
-    Route::post('/types/{id}',      [ProductTypeController::class, 'update']);
-    Route::delete('/types/{id}',    [ProductTypeController::class, 'delete']);
-});

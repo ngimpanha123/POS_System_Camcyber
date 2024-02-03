d7a9e59abb830c08e2471eb65b5fe03ac409a61b
Khouch Koeun <khouch.koeun@gmail.com>
2024-02-04 02:41:16 +0700

    remove comment for telegram.php in config

--- config/telegram.php
+++ config/telegram.php
@@ -3,47 +3,6 @@
 use Telegram\Bot\Commands\HelpCommand;
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Your Telegram Bots
-    |--------------------------------------------------------------------------
-    | You may use multiple bots at once using the manager class. Each bot
-    | that you own should be configured here.
-    |
-    | Here are each of the telegram bots config parameters.
-    |
-    | Supported Params:
-    |
-    | - name: The *personal* name you would like to refer to your bot as.
-    |
-    |       - token:    Your Telegram Bot's Access Token.
-                        Refer for more details: https://core.telegram.org/bots#botfather
-    |                   Example: (string) '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'.
-    |
-    |       - commands: (Optional) Commands to register for this bot,
-    |                   Supported Values: "Command Group Name", "Shared Command Name", "Full Path to Class".
-    |                   Default: Registers Global Commands.
-    |                   Example: (array) [
-    |                       'admin', // Command Group Name.
-    |                       'status', // Shared Command Name.
-    |                       Acme\Project\Commands\BotFather\HelloCommand::class,
-    |                       Acme\Project\Commands\BotFather\ByeCommand::class,
-    |             ]
-    */
-    // 'bots' => [
-    //     'mybot' => [
-    //         'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
-    //         'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-    //         'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
-    //         'commands' => [
-    //             //Acme\Project\Commands\MyTelegramBot\BotCommand::class
-    //         ],
-    //     ],
-
-    //     //        'mySecondBot' => [
-    //     //            'token' => '123456:abc',
-    //     //        ],
-    // ],
 
     'bot_token' => env('TELEGRAM_BOT_TOKEN'),
     /*
@@ -149,46 +108,7 @@ return [
     | Examples shown below are by the group type for you to understand each of them.
     */
     'command_groups' => [
-        /* // Group Type: 1
-           'commmon' => [
-                Acme\Project\Commands\TodoCommand::class,
-                Acme\Project\Commands\TaskCommand::class,
-           ],
-        */
 
-        /* // Group Type: 2
-           'subscription' => [
-                'start', // Shared Command Name.
-                'stop', // Shared Command Name.
-           ],
-        */
-
-        /* // Group Type: 3
-            'auth' => [
-                Acme\Project\Commands\LoginCommand::class,
-                Acme\Project\Commands\SomeCommand::class,
-            ],
-
-            'stats' => [
-                Acme\Project\Commands\UserStatsCommand::class,
-                Acme\Project\Commands\SubscriberStatsCommand::class,
-                Acme\Project\Commands\ReportsCommand::class,
-            ],
-
-            'admin' => [
-                'auth', // Command Group Name.
-                'stats' // Command Group Name.
-            ],
-        */
-
-        /* // Group Type: 4
-           'myBot' => [
-                'admin', // Command Group Name.
-                'subscription', // Command Group Name.
-                'status', // Shared Command Name.
-                'Acme\Project\Commands\BotCommand' // Full Path to Command Class.
-           ],
-        */
     ],
 
     /*
@@ -208,8 +128,5 @@ return [
     |
     */
     'shared_commands' => [
-        // 'start' => Acme\Project\Commands\StartCommand::class,
-        // 'stop' => Acme\Project\Commands\StopCommand::class,
-        // 'status' => Acme\Project\Commands\StatusCommand::class,
     ],
 ];

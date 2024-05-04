b8bb35f48ed214f3f6d43d6ffed2eb2c8840d88c
panha <ngimpanha0.com>
2024-05-05 02:59:24 +0700

    Create function update for product type

--- app/Http/Controllers/Admin/ProductTypeController.php
+++ app/Http/Controllers/Admin/ProductTypeController.php
@@ -40,4 +40,43 @@ class ProductTypeController extends Controller
         ], Response::HTTP_OK);
     }
 
+    public function update(Request $req, $id = 0)
+    {
+        // ===>> Check validation
+        $this->validate(
+            $req,
+            [
+                'name'             => 'required|max:20',
+            ],
+            [
+                'name.required'    => 'សូមបញ្ចូលឈ្មោះម៉ាកផលិតផល',
+                'name.max'         => 'ឈ្មោះម៉ាកផលិតផលមិនអាចលើសពី២០ខ្ទង់',
+            ]
+        );
+
+        //==============================>> Start updating data
+        $data           =   Type::find($id);
+
+        if ($data) {
+
+            $data->name     =   $req->name;
+            $data->save();
+
+            return response()->json([
+                'status'        => 'ជោគជ័យ',
+                'message'       => 'ប្រភេទផលិតផលត្រូវបានកែប្រែជោគជ័យ!',
+                'data'          => $data,
+            ], Response::HTTP_OK);
+
+        }else {
+
+            return response()->json([
+                'status'    => 'បរាជ័យ',
+                'message'   => 'ទិន្នន័យមិនត្រឹមត្រូវ',
+            ], Response::HTTP_BAD_REQUEST);
+
+        }
+    }
+
+
 }

6ef3ec685cb09308c98bf916eedf54d4c258f424
panha <ngimpanha0.com>
2024-05-05 02:22:14 +0700

    Create ProductController

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\MainController;
+use App\Models\Products\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+}

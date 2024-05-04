85ce8b053a58203a0a53fafd00ece4084807fdfa
panha <ngimpanha0.com>
2024-05-05 02:25:16 +0700

    Create function getData for product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -10,4 +10,26 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     //
+    public function getData(Request $request){
+        
+        //declare variable
+        $data = Product::select('*')->with(['type']);
+
+        if($request->key && $request->Key != ''){
+
+            $data = $data->where('code', 'LIKE', '%' . $request->Key . '%')->OrWhere('name', 'LIKE', '%' . $request->Key .'%');
+
+        }
+
+        if($request->type && $request->type != 0){
+
+            $data = $data->where('type_id', $request->type);
+        }
+
+        $data = $data->orderBy('id','desc')->paginate($request->limit ? $request->limit :10,'per_page');
+
+        return response()->json($data,Response::HTTP_OK);
+
+    }
+
 }
